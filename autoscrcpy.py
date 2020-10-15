35bd212283368aa4f886235ad863add7f6e54f11
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2020-10-15 09:28:19 +0530

    Added base code

---
+++ autoscrcpy.py
@@ -0,0 +1,22 @@
+import time, os
+from ppadb.client import Client
+
+# Default is "127.0.0.1" and 5037
+client = Client(host="127.0.0.1", port=5037)
+
+def runner():
+    dev_num = 0
+    while True:
+        devices = client.devices()
+
+        if dev_num != len(devices):
+            print(devices)
+            if len(devices) == 0:
+                pass
+            else:
+                os.system('scrcpy')
+
+        dev_num = len(devices)
+        time.sleep(3)
+
+runner()
